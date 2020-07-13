b572975a20fe47a15c30e13197089d81341b20de
Leonardo Fernandez <65931925+LeoFernandezR@users.noreply.github.com>
2020-07-12 22:56:11 -0300

    Update bd_conexion.php

--- inc/funciones/bd_conexion.php
+++ inc/funciones/bd_conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USUARIO', 'sw5MxXGW5u');
-define('DB_PASSWORD', 'pjZJI2ypmy');
-define('DB_HOST', 'remotemysql.com');
-define('DB_NOMBRE', 'sw5MxXGW5u');
+define('DB_USUARIO', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+define('DB_NOMBRE', '');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NOMBRE, 3306);
 $conn->set_charset('utf8');
